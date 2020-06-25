9f3e4d00f71fcbf725250a75c28dd5f6339ee075
Dan Sumption <dan@sumption.org>
2020-06-25 23:17:22 +0100

    Hide extra layers

--- src/index.js
+++ src/index.js
@@ -13,23 +13,23 @@ let counter;
 const setupLayers = function(layers) {
   // layers.push(new Layer({ name: 'blobs', ...flowersMixin }));
   // layers.push(new Layer({ name: 'img', ...imgMixin }));
-  layers.push(
-    new Layer({
-      name: 'queenimage',
-      src: '../img/ice/queen/queen',
-      format: 'jpg',
-      numImages: 1,
-      max: 1,
-      newImgChance: 0.23,
-      glitchChance: 0.23,
-      visible: true,
-      grow: true,
-      showPercentage: 25,
-      hidePercentage: 1,
-      beatMultiplier: 1,
-      ...imgMixin
-    })
-  );
+  // layers.push(
+  //   new Layer({
+  //     name: 'queenimage',
+  //     src: '../img/ice/queen/queen',
+  //     format: 'jpg',
+  //     numImages: 1,
+  //     max: 1,
+  //     newImgChance: 0.23,
+  //     glitchChance: 0.23,
+  //     visible: true,
+  //     grow: true,
+  //     showPercentage: 25,
+  //     hidePercentage: 1,
+  //     beatMultiplier: 1,
+  //     ...imgMixin
+  //   })
+  // );
   layers.push(new Layer({ name: 'threads', ...threadsMixin }));
   // layers.push(
   //   new Layer({
@@ -44,39 +44,38 @@ const setupLayers = function(layers) {
     new Layer({
       name: 'worms',
       visible: true,
-      showPercentage: 4,
+      showPercentage: 23,
       hidePercentage: 2,
       ...wormsMixin
     })
   );
-
-  layers.push(
-    new Layer({
-      name: 'spin',
-      visible: false,
-      showPercentage: 1,
-      hidePercentage: 1.5,
-      beatMultiplier: 4,
-      renderHidden: true,
-      ...spinMixin
-    })
-  );
-  layers.push(
-    new Layer({
-      name: 'img2',
-      src: '../img/ice/image',
-      format: 'jpg',
-      numImages: 29,
-      max: 29,
-      newImgChance: 0.23,
-      glitchChance: 0.69,
-      visible: true,
-      showPercentage: 0.001,
-      hidePercentage: 3,
-      beatMultiplier: 100000,
-      ...imgMixin
-    })
-  );
+  // layers.push(
+  //   new Layer({
+  //     name: 'spin',
+  //     visible: false,
+  //     showPercentage: 1,
+  //     hidePercentage: 1.5,
+  //     beatMultiplier: 4,
+  //     renderHidden: true,
+  //     ...spinMixin
+  //   })
+  // );
+  // layers.push(
+  //   new Layer({
+  //     name: 'img2',
+  //     src: '../img/ice/image',
+  //     format: 'jpg',
+  //     numImages: 29,
+  //     max: 29,
+  //     newImgChance: 0.23,
+  //     glitchChance: 0.69,
+  //     visible: true,
+  //     showPercentage: 0.001,
+  //     hidePercentage: 3,
+  //     beatMultiplier: 100000,
+  //     ...imgMixin
+  //   })
+  // );
   // layers.push(
   //   new Layer({
   //     name: 'blob flowers',
